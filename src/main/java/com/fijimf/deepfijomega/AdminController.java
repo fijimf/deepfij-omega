27486950322b3725a7903f05e5771c79f8fe663e
Jim <fijimf@gmail.com>
2020-11-14 13:53:38 -0500

    User screen done

--- src/main/java/com/fijimf/deepfijomega/AdminController.java
+++ src/main/java/com/fijimf/deepfijomega/AdminController.java
@@ -1,13 +1,15 @@
 package com.fijimf.deepfijomega;
 
-import com.fijimf.deepfijomega.entity.user.User;
 import com.fijimf.deepfijomega.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 
 @Controller
 public class AdminController {
@@ -15,13 +17,66 @@ public class AdminController {
     UserRepository userRepository;
 
     @GetMapping("/admin")
-    public String signupForm(Model model) {
+    public String adminMenu(Model model) {
         return "admin/adminMenu";
     }
 
     @GetMapping("/admin/users")
     public String manageUsers(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        return("admin/users");
+        return ("admin/users");
+    }
+
+    @GetMapping("/force-active/{id}")
+    public ModelAndView manageUsers(Model model, @PathVariable("id") Long id) {
+        userRepository.findById(id).ifPresent(u -> {
+            u.setActivated(true);
+            userRepository.save(u);
+        });
+        return new ModelAndView("redirect:/admin/users");
+    }
+
+    @GetMapping("/lock/{id}")
+    public ModelAndView lock(Model model, @PathVariable("id") Long id) {
+        userRepository.findById(id).ifPresent(u -> {
+            u.setLocked(true);
+            userRepository.save(u);
+        });
+        return new ModelAndView("redirect:/admin/users");
+    }
+
+    @GetMapping("/unlock/{id}")
+    public ModelAndView unlock(Model model, @PathVariable("id") Long id) {
+        userRepository.findById(id).ifPresent(u -> {
+            u.setLocked(false);
+            userRepository.save(u);
+        });
+        return new ModelAndView("redirect:/admin/users");
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView delete(Model model, @PathVariable("id") Long id) {
+        userRepository.deleteById(id);
+        return new ModelAndView("redirect:/admin/users");
+    }
+
+    @GetMapping("/no-expire/{id}")
+    public ModelAndView clearExpiry(Model model, @PathVariable("id") Long id) {
+        userRepository.findById(id).ifPresent(u -> {
+            u.setExpireCredentialsAt(null);
+            userRepository.save(u);
+        });
+        return new ModelAndView("redirect:/admin/users");
+    }
+
+    @GetMapping(value = "/set-expire/{id}", params = {"minutes"})
+    public ModelAndView setExpiry(Model model, @PathVariable("id") Long id, @Param("minutes") Long minutes) {
+        userRepository.findById(id).ifPresent(u -> {
+            if (minutes != null && minutes > 0L) {
+                u.setExpireCredentialsAt(LocalDateTime.now().plusMinutes(minutes));
+                userRepository.save(u);
+            }
+        });
+        return new ModelAndView("redirect:/admin/users");
     }
 }
