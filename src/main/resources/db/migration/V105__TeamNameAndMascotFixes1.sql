bf2e405c125ddad4f9d17464b2ac34416e602eeb
Jim <fijimf@gmail.com>
2021-01-04 00:15:03 -0500

    Minor bug fix favicon; Data edit for team names

---
+++ src/main/resources/db/migration/V105__TeamNameAndMascotFixes1.sql
@@ -0,0 +1,10 @@
+
+update team set nickname='Bulldogs' where key='gonzaga';
+update team set nickname='Huskies' where key='uconn';
+update team set nickname='Terrapins' where key='maryland';
+update team set nickname='Cornhuskers' where key='nebraska';
+update team set nickname='Bulldogs' where key='yale';
+
+update team set name='TX A&M-Corpus Christi' where key = 'am-corpus-chris';
+update team set name='Army' where key = 'army';
+update team set name='Coastal Carolina' where key = 'coastal-caro';
